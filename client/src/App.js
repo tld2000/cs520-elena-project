cfd794841f35de5fd46cf44c285c72ec52b719cf
kazuhaokamoto <kazuhaokamoto2018@gmail.com>
2022-04-19 20:51:17 -0400

    Created base website with map

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2F6dWhhb2thbW90byIsImEiOiJjbDF2NjhmMm8yZjY4M2Ntb3hsOGRibWtkIn0.s0CQAwqbmc-DTF7E9vkm1w';
+ 
+export default function App() {
+const mapContainer = useRef(null);
+const map = useRef(null);
+const [lng, setLng] = useState(-70.9);
+const [lat, setLat] = useState(42.35);
+const [zoom, setZoom] = useState(9);
+const geocoder = useRef(null);
+
+useEffect(() => {
+if (map.current) return; // initialize map only once
+map.current = new mapboxgl.Map({
+container: mapContainer.current,
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [lng, lat],
+zoom: zoom
+});
+geocoder.current = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+    });
+map.current.addControl(geocoder.current,"top-right")
+
+});
+
+useEffect(() => {
+if (!map.current) return; // wait for map to initialize
+map.current.on('move', () => {
+setLng(map.current.getCenter().lng.toFixed(4));
+setLat(map.current.getCenter().lat.toFixed(4));
+setZoom(map.current.getZoom().toFixed(2));
+});
+
+
+});
+
+
+return (
+<div>
+    <div className="sidebar">
+    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+    </div>
+    <div ref={mapContainer} className="map-container" />
+</div>
+);
+}
