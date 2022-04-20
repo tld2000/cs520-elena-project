4dd468bf3418912198d606e5fa22c3b3f5262d13
kazuhaokamoto <kazuhaokamoto2018@gmail.com>
2022-04-19 20:51:17 -0400

    Created base website with map

---
+++ client/src/components/navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+ 
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+ 
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+
+// Here, we display our Navbar
+export default function Navbar() {
+ return (
+   <div>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+       <NavLink className="navbar-brand" to="/">
+       <img style={{"width" : 15 + '%'}} src="https://www.pinclipart.com/picdir/big/544-5443703_transparent-oval-clipart-earth-clipart-vector-png-download.png"></img>
+       </NavLink>
+       <button
+         className="navbar-toggler"
+         type="button"
+         data-toggle="collapse"
+         data-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span className="navbar-toggler-icon"></span>
+       </button>
+ 
+       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav ml-auto">
+           <li className="nav-item">
+             <NavLink className="nav-link" to="/create">
+               Create Record
+             </NavLink>
+           </li>
+         </ul>
+       </div>
+     </nav>
+   </div>
+ );
+}
