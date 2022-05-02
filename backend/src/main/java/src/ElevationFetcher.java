a296038e42a841c7ee2e5c5d4d1fdb95c7b37850
tld2000 <51632539+tld2000@users.noreply.github.com>
2022-05-02 06:28:24 -0400

    added elevation fetcher backend

--- backend/src/main/java/src/ElevationFetcher.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-import org.springframework.core.SpringVersion;
-
-public class ElevationFetcher {
-    public static void main(String[] args) {
-        System.out.println(SpringVersion.getVersion());
-    }
-}
