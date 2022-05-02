0e9ccf552d20fbe787bb4fa8e397ae6bbc58bfab
tld2000 <51632539+tld2000@users.noreply.github.com>
2022-05-01 20:41:21 -0400

    added file structure

---
+++ backend/src/main/java/src/ElevationFetcher.java
@@ -0,0 +1,9 @@
+package src;
+
+import org.springframework.core.SpringVersion;
+
+public class ElevationFetcher {
+    public static void main(String[] args) {
+        System.out.println(SpringVersion.getVersion());
+    }
+}
